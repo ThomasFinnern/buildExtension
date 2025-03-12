4f4b7fa69db03666ef1fa0246e7681e56a567ffe
ThomasFinnern <ThomasFinnern@users.noreply.github.com>
2025-03-12 21:29:06 +0100

    option files for version with example.bat

---
+++ src/options_version_tsk/example.bat
@@ -0,0 +1,68 @@
+@ECHO OFF
+REM build_develop.bat
+REM options file can be selected 
+REM d:develop, s:step, f:fix, r:release, m:major
+CLS
+
+REM Path for calling
+set ExePath=C:\Program Files\php82\
+REM ECHO ExePath: "%ExePath%"
+
+if exist "%ExePath%php.exe" (
+    REM path known (WT)
+    ECHO ExePath: "%ExePath%"
+) else (
+    REM Direct call
+    ECHO PHP in path variable
+    set ExePath=
+)
+
+REM "C:\Program Files\php82\php.exe" --version
+"%ExePath%php.exe" --version
+
+ECHO ----------------------------------------------
+ECHO.
+
+set OptionFile=
+
+if %1==-d (
+	set OptionFile=-o options_version_tsk\build_develop.opt
+)
+
+if %1==-s (
+	set OptionFile=-o options_version_tsk\build_step.opt
+)
+
+if %1==-f (
+	set OptionFile=-o options_version_tsk\build_fix.opt
+)
+
+if %1==-r (
+	set OptionFile=-o options_version_tsk\build_release.opt
+)
+
+if %1==-m (
+	set OptionFile=-o options_version_tsk\build_major.opt
+)
+
+
+ECHO ----------------------------------------------
+ECHO.
+
+pushd  ..\..\buildExtension\src
+ECHO Path: %cd% 
+
+echo --- "%ExePath%php.exe" buildExtensionCmd.php -f ../../LangMan4DevProject/.buildPHP/build.tsk %1 %OptionFile%
+"%ExePath%php.exe" buildExtensionCmd.php -f ../../LangMan4DevProject/.buildPHP/build.tsk %1 %OptionFile%
+popd
+
+GOTO :EOF
+
+REM ------------------------------------------
+REM Adds given argument to the already known command arguments
+:AddNextArg
+    Set NextArg=%*
+    Set CmdArgs=%CmdArgs% %NextArg%
+    ECHO  '%NextArg%'
+GOTO :EOF
+

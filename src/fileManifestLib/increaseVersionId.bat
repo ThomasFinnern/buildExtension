44efb02c3bd6389219bc63b815f3f912589c5325
ThomasFinnern <ThomasFinnern@users.noreply.github.com>
2025-03-20 11:54:46 +0100

    *.bat improved comparison if %1A==-dA (

--- src/fileManifestLib/increaseVersionId.bat
+++ src/fileManifestLib/increaseVersionId.bat
@@ -25,24 +25,24 @@ ECHO.
 
 set OptionFile=
 
-if %1==-d (
-	set OptionFile=-o ..\options_version_tsk\build_develop.opt
+if %1A==-dA (
+	set OptionFile=-o options_version_tsk\build_develop.opt
 )
 
-if %1==-s (
-	set OptionFile=-o ..\options_version_tsk\build_step.opt
+if %1A==-sA (
+	set OptionFile=-o options_version_tsk\build_step.opt
 )
 
-if %1==-f (
-	set OptionFile=-o ..\options_version_tsk\build_fix.opt
+if %1A==-fA (
+	set OptionFile=-o options_version_tsk\build_fix.opt
 )
 
-if %1==-r (
-	set OptionFile=-o ..\options_version_tsk\build_release.opt
+if %1A==-rA (
+	set OptionFile=-o options_version_tsk\build_release.opt
 )
 
-if %1==-m (
-	set OptionFile=-o ..\options_version_tsk\build_major.opt
+if %1A==-mA (
+	set OptionFile=-o options_version_tsk\build_major.opt
 )
 
 
--- src/options_version_tsk/example.bat
+++ src/options_version_tsk/example.bat
@@ -25,20 +25,24 @@ ECHO.
 
 set OptionFile=
 
-if %1==-d (
+if %1A==-dA (
 	set OptionFile=-o options_version_tsk\build_develop.opt
 )
 
-if %1==-s (
+if %1A==-sA (
 	set OptionFile=-o options_version_tsk\build_step.opt
 )
 
-if %1==-f (
+if %1A==-fA (
 	set OptionFile=-o options_version_tsk\build_fix.opt
 )
 
-if %1==-r (
+if %1A==-rA (
 	set OptionFile=-o options_version_tsk\build_release.opt
+)
+
+if %1A==-mA (
+	set OptionFile=-o options_version_tsk\build_major.opt
 )
 
 if %1==-m (
